6f5ae174edf3b42522d80f13e1bf0971ce51a450
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-09-07 13:32:46 +0200

    lesson update

--- basic_programming/lesson_03/code/homework/Methods.java
+++ basic_programming/lesson_03/code/homework/Methods.java
@@ -1,16 +1,18 @@
 package homework;
 
 public class Methods {
-    public static void main(String[] args) {
-//        1. Метод main вызывает method1. method1 вызывает method2. method2 вызывает method3.
+
+    //        1. Метод main вызывает method1. method1 вызывает method2. method2 вызывает method3.
 //        2. В каждом методе должно происходить следующее:
 //        + Печатает в консоль информацию, что метод такой-то стартовал
 //                + Вызывает следующий метод
 //        + Печатает в консоль, что метод такой-то финишировал
 
+    public static void main(String[] args) {
         method1();
 
     }
+
     public static void method1(){
         System.out.println("Method 1 is starting.");
         method2();
---
+++ basic_programming/lesson_03/code/variables/OperationsAppl.java
@@ -0,0 +1,20 @@
+package variables;
+
+public class OperationsAppl {
+
+    public static void main(String[] args) {
+        // Operations with integer numbers
+        int x = 20; // объявили переменную
+        int y = 10;
+        int z = x + y; // выполнили сложение
+
+        System.out.println("Sum X: " + x + " + Y: " + y + " = " + z);
+
+        System.out.println(x + ", " + y +", " + z);
+
+        z = x * y;
+        System.out.println(z);
+
+    }
+
+}
