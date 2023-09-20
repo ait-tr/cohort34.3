db0395883f89b1a3dd4698f7d8d725cdeeb67685
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-09-20 13:08:50 +0200

    lesson update

--- basic_programming/lesson_12/code/homework/ArrayMethods.java
+++ basic_programming/lesson_12/code/homework/ArrayMethods.java
@@ -19,43 +19,45 @@ package homework;
 public class ArrayMethods {
     public static void main(String[] args) {
 
-        int[] myArray = new int[10];
+        int[] myArray = new int[10]; // объявляем массив
         int a = -10;
         int b = 10;
-        fillArray(myArray, -10, 10);
-        printArray(myArray);
-        System.out.println("=================================");
-        System.out.println("Sum of odd elements is = " + sumOfOddElements(myArray));
-        System.out.println("Multiplication of elements with even indexes is: " + multOfElEvenIndex(myArray));
-        System.out.println("=================================");
-        reverseArrray(myArray);
-        printArray(myArray);
-    }
+        fillArray(myArray, -10, 10); // вызов метода, fill - это заполнить
+        printArray(myArray); // вызов метода,
 
+          System.out.println("=================================");
+          System.out.println("Sum of odd elements is = " + sumOfOddElements(myArray)); // Even - четные, Odd - нечетные числа
+          System.out.println("Multiplication of elements with even indexes is: " + multOfElEvenIndex(myArray));
+          System.out.println("=================================");
+          reverseArrray(myArray);
+          printArray(myArray);
+    } // ============end of main =============================
+
+    // ======================Methods==========================
     // reverse array
     public static void reverseArrray(int[] arr){
         int temp = 0;
         for (int i = 0; i < arr.length/2 ; i++) {
-            temp = arr[i];
-            arr[i] = arr[arr.length - 1 - i];
-            arr[arr.length - 1 - i] = temp;
+            temp = arr[i]; // берем левый элемент во временную переменную
+            arr[i] = arr[arr.length - 1 - i]; // переносим правый элемент на место левого
+            arr[arr.length - 1 - i] = temp; // место правого ставим тот, что во временной переменной
         }
     }
-
+    // ...
     public static int multOfElEvenIndex(int[] arr){
         int mult = 1;
         for (int i = 0; i < arr.length; i++) {
-            if(i % 2 == 0){
+            if(i % 2 == 0){ // проверяем индекс на четность
                 mult *= arr[i];
             }
         }
         return mult;
     }
-
+    // ...
     public static int sumOfOddElements(int[] arr){
         int sumOfOddEl = 0;
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i] % 2 != 0){
+            if(arr[i] % 2 != 0){  // проверка элемента массива на четность
                 sumOfOddEl += arr[i];
             }
         }
@@ -64,8 +66,8 @@ public class ArrayMethods {
 
     // filling array
     public static void fillArray(int[] arr, int a, int b) {
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (int) (Math.random() * (b - a + 1) + a);
+        for (int i = 0; i < arr.length; i++) {  // обходим, пробегаем все элементы массива
+            arr[i] = (int) (Math.random() * (b - a + 1) + a); // в элемент массива кладем случайное целое число
         }
     }
 
@@ -76,5 +78,5 @@ public class ArrayMethods {
         }
         System.out.println();
     }
-
+ // ===========end of Methods ===================
 }
