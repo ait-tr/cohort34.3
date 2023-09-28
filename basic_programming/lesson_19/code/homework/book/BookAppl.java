3dde2efe6303c11e462fdb37024828c4439f7059
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-09-28 20:54:42 +0200

    lesson update

---
+++ basic_programming/lesson_19/code/homework/book/BookAppl.java
@@ -0,0 +1,17 @@
+package homework.book;
+
+public class BookAppl {
+    public static void main(String[] args) {
+
+        Book book = new Book("War and peace", "Tolstoy", 1980, 1000L);
+        book.display();
+        System.out.println("----------------------------------------");
+        Dictionary dictionaryEng = new Dictionary("English Dictionary", "Bonk", 1985, 2000L, "english", 20000, "language");
+        dictionaryEng.display();
+        System.out.println("----------------------------------------");
+        Dictionary dictionaryWords = new Dictionary("Words Dictionary", "Ojegov", 1990, 3000L, "russian", 30000, "words");
+        dictionaryWords.display();
+        System.out.println("----------------------------------------");
+
+    }
+}
