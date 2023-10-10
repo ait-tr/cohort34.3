88fc4d98896276acbcbe1353e084429f547ec578
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-10-10 13:43:09 +0200

    lesson update

--- basic_programming/lesson_25/code/practice/StringAppl.java
+++ basic_programming/lesson_25/code/practice/StringAppl.java
@@ -1,5 +1,7 @@
 package practice;
 
+import java.util.Arrays;
+
 public class StringAppl {
     public static void main(String[] args) {
 
@@ -40,6 +42,27 @@ public class StringAppl {
         System.out.println(index);
         index = str.lastIndexOf('l'); // индекс последнего нахождения l
         System.out.println(index);
+        index = str.indexOf('A'); // ищем индекс отсутствующей буквы
+        System.out.println(index); // получаем -1
+        System.out.println("-----------------------");
+        System.out.println(str.substring(4)); // печатаем подстроку с индекса
+        System.out.println(str.substring(1,8)); // печатаем подстроку с индекса до индекса
+        System.out.println((str.replace("o", "o-o-o"))); // замена
+        System.out.println("-----------------------");
+        String str4 = "           Bill      ";
+        System.out.println(str4);
+        str4 = str4.trim(); // убираем пробелы
+        System.out.println(str4);
+        System.out.println("-----------------------");
+        String str5 = "I love String in Java!";
+        String[] words = str5.split(" "); // разделяем строку на элементы массива типа String
+        System.out.println("Quantity of words in string: " + words.length); // узнали длину массива
+        System.out.println(Arrays.toString(words)); // напечатали массив
+        System.out.println("-----------------------");
+        String str6 = "Я помню чудное мгновенье...";
+        String[] letters = str6.split(""); // разделителя нет, чтобы получить буквы
+        System.out.println("Quantity of letters in string: " + letters.length); // узнали длину массива
+        System.out.println(Arrays.toString(letters)); // напечатали массив
 
     }
 }
