f533a2ff57923cb43b358660a8e4a51b09999754
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-10-18 14:16:27 +0200

    lesson update

--- basic_programming/lesson_31/code/practice/company_v1/dao/CompanyImpl.java
+++ basic_programming/lesson_31/code/practice/company_v1/dao/CompanyImpl.java
@@ -1,10 +1,11 @@
 package practice.company_v1.dao;
 
 import practice.company_v1.model.Employee;
+import practice.company_v1.model.SalesManager;
 
 public class CompanyImpl implements Company {
     // поля, которые описывают компанию
-    private Employee[] employees; // массив для хранения сотрудников
+    private Employee[] employees; // сотрудники - массив для хранения сотрудников
     private int size; // текущее кол-во сотрудников в компании
 
     // конструктор
@@ -14,44 +15,76 @@ public class CompanyImpl implements Company {
 
     @Override
     public boolean addEmployee(Employee employee) {
-        return false;
+        // не добавляем null, не превышаем capacity, не добавляем уже существующий элемент
+        if( employee == null || size == employees.length || findEmployee(employee.getId()) != null) {
+            return false;
+        }
+        employees[size] = employee; // новый элемент
+        size++;
+        return true;
     }
 
     @Override
     public Employee removeEmployee(int id) {
+        for (int i = 0; i < size; i++) {
+            if(employees[i].getId() == id){
+                Employee victim = employees[i]; // убрали найденный элемент в в переменную
+                employees[i] = employees[size - 1]; // на место найденного поставили последнего существ. в массиве
+                employees[size - 1] = null; // обнулили последнего
+                size--;
+                // можно написать короче этот код TODO
+                return victim;
+            }
+        }
         return null;
     }
 
     @Override
     public Employee findEmployee(int id) {
+        for (int i = 0; i < size; i++) {
+            if(employees[i].getId() == id){ // нашелся элемент массива, у которого совпал id
+                return employees[i]; // вернули найденный элемент массива типа Employee
+            }
+        }
         return null;
     }
 
     @Override
     public int quantity() {
-        return 0;
+        return size;
     }
 
     // удалить
     @Override
     public double totalSalary() {
-        return 0;
+        double res = 0;
+        for (int i = 0; i < size; i++) {
+            res += employees[i].calcSalary();
+        }
+        return res;
     }
 
     @Override
     public double avgSalary() {
-
-        return 0;
+        return totalSalary() / size;
     }
 
     @Override
     public double totalSales() {
-
-        return 0;
+        double res = 0;
+        for (int i = 0; i < size; i++) {
+            if(employees[i] instanceof SalesManager){
+                SalesManager salesManager = (SalesManager) employees[i]; // провели кастинг
+                res += salesManager.getSalesValue();
+            }
+        }
+        return res;
     }
 
     @Override
     public void printEmployees() {
-
+        for (int i = 0; i < size; i++) {
+            System.out.println(employees[i]);
+        }
     }
 }
--- basic_programming/lesson_31/code/practice/company_v1/tests/CompanyImplTest.java
+++ basic_programming/lesson_31/code/practice/company_v1/tests/CompanyImplTest.java
@@ -14,17 +14,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class CompanyImplTest {
 
     Company company;
-    Employee[] employee;
+    Employee[] e;
 
     @BeforeEach
     void setUp() {
         company = new CompanyImpl(5);
-        employee = new Employee[4];
-        employee[0] = new Manager(101, "John", "Smith", 45, 160, 5000, 5);
-        employee[1] = new SalesManager(102, "Anna", "Black", 36, 160, 25000, 0.1);
-        employee[2] = new SalesManager(103, "Thomas", "White", 28, 160, 30000, 0.1);
-        employee[3] = new Worker(104, "Gans", "Bauer", 30, 160, 5);
+        e = new Employee[4];
+        e[0] = new Manager(101, "John", "Smith", 45, 160, 5000, 5);
+        e[1] = new SalesManager(102, "Anna", "Black", 36, 160, 25000, 0.1);
+        e[2] = new SalesManager(103, "Thomas", "White", 28, 160, 30000, 0.1);
+        e[3] = new Worker(104, "Gans", "Bauer", 30, 160, 5);
 
+        // добавим элементы массива в company
+        for (int i = 0; i < e.length; i++) {
+            company.addEmployee(e[i]);
+        }
     }
 
     @Test
@@ -32,7 +36,7 @@ class CompanyImplTest {
         // не можем добавить null - пустой объект
         assertFalse(company.addEmployee(null));
         // не можем добавить второй раз, уже существующего работника
-        assertFalse(company.addEmployee(employee[1]));
+        assertFalse(company.addEmployee(e[1]));
         Employee employee1 = new Manager(105, "Ivan", "Dubin", 55, 160, 6000, 6); // создали нового сотрудника
         assertTrue(company.addEmployee(employee1)); // добавили нового сотрудника
         assertEquals(5, company.quantity()); // теперь в компании 5 сотрудников
@@ -44,7 +48,7 @@ class CompanyImplTest {
     @Test
     void removeEmployee() {
         // удаляем сотрудника
-        assertEquals(employee[1], company.removeEmployee(102));
+        assertEquals(e[1], company.removeEmployee(102));
         assertEquals(3, company.quantity()); // сотрудников стало на 1 меньше (4 - 1)
         assertNull(company.removeEmployee(102)); // дважды не можем удалить
         assertNull(company.findEmployee(102)); // не можем найти после удаления
@@ -53,32 +57,34 @@ class CompanyImplTest {
     @Test
     void findEmployee() {
         // ищем сотрудника по id
-        assertEquals(employee[1], company.findEmployee(102));
+        assertEquals(e[1], company.findEmployee(102));
         // ищем несуществующего сотрудника
         assertNull(company.findEmployee(105));
     }
 
     @Test
-    void quantity() {
+    void quantityTest() {
+        assertEquals(4, company.quantity());
 
     }
 
     @Test
-    void totalSalary() {
-
+    void totalSalaryTest() {
+        assertEquals(12100., company.totalSalary(), 0.01);
     }
 
     @Test
-    void avgSalary() {
-
+    void avgSalaryTest() {
+        assertEquals( 12100.0 / 4, company.avgSalary(), 0.01);
     }
 
     @Test
     void totalSales() {
+        assertEquals(55000, company.totalSales());
     }
 
     @Test
     void printEmployees() {
-
+        company.printEmployees();
     }
 }
