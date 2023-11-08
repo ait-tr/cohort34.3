4b89deafb5200422f05d66d284f23c37dca1d6d9
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-11-08 13:22:26 +0100

    lesson update

--- basic_programming/lesson_31/code/homework/library/Library.java
+++
@@ -1,9 +0,0 @@
-package homework.library;
-
-public interface Library {
-    boolean addBook(Book book);
-    Book removeBook(long isbn);
-    Book findBook(long isbn);
-    int quantity();
-    void printBook();
-}
