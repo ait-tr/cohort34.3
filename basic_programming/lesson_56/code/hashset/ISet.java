4c523c0484ad7183bd35b449427447ba4c0f1ad1
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-11-29 13:52:49 +0100

    lesson update

---
+++ basic_programming/lesson_56/code/hashset/ISet.java
@@ -0,0 +1,8 @@
+package hashset;
+
+public interface ISet<E> extends Iterable<E> {
+    boolean add (E element);
+    boolean contains(E element);
+    boolean remove (E element);
+    int size();
+}
