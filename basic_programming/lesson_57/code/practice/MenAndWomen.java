fe2d706afc4b4c4f9a66c4d210a1184873db505c
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-11-30 17:45:57 +0100

    lesson update

--- basic_programming/lesson_57/code/practice/MenAndWomen.java
+++ basic_programming/lesson_57/code/practice/MenAndWomen.java
@@ -11,11 +11,37 @@ public class MenAndWomen {
         System.out.println("Group list with gender (sex)");
 
         Map<String, String> groupMapGender = new HashMap<>();
-        groupMapGender.put("Leonid", "male");
-        groupMapGender.put("Sergey1", "male");
-        groupMapGender.put("Galina", "female");
+        groupMapGender.put("Leonid", "Male");
+        groupMapGender.put("Kirill", "Male");
+        groupMapGender.put("Olesia", "Female");
+        groupMapGender.put("Galina", "Female");
+        groupMapGender.put("Andreas", "Male");
+        groupMapGender.put("Sergei", "Male");
+        groupMapGender.put("Elena", "Female");
+        groupMapGender.put("Victor", "Male");
+        groupMapGender.put("Tatyana", "Female");
+        groupMapGender.put("Vladislava", "Female");
+        groupMapGender.put("Iryna", "Female");
+        groupMapGender.put("Sasha", "Male");
+        groupMapGender.put("Olga", "Female");
+        groupMapGender.put("Olg", "Female");
 
-        
+        int women = 0;
+        for (String name : groupMapGender.keySet()) { // проход по ключам
+            if(groupMapGender.get(name).equals("Female")) {
+                women++;
+            }
+        }
+        System.out.println("Women : " + women);
+
+        women = 0;
+        for (String name : groupMapGender.values()) { // по коллекции значений
+            if(name.equals("Female")) {
+                women++;
+            }
+        }
+        System.out.println("Women : " + women);
+
 
     }
 }
