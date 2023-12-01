b55a8694c0084eb6928bd36bb5eea90979f18350
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-12-01 13:08:55 +0100

    lesson update

--- basic_programming/lesson_58/code/practice/stream_intro/StreamAppl.java
+++ basic_programming/lesson_58/code/practice/stream_intro/StreamAppl.java
@@ -7,51 +7,38 @@ import java.util.stream.Stream;
 
 public class StreamAppl {
     public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList<>(List.of(1, 9, 2, 8, 3, 7, 4, 6, 5)); // определили список целых чисел
-        numbers.stream() // подключили к нему метод stream
-                .filter(a -> a > 3) // отбираем элементы по условию
-                .peek(a -> System.out.println("peek before sort: " + a)) // This method exists mainly to support debugging
-                .sorted((n1, n2) -> -Integer.compare(n1, n2)) // сортируем
-                .forEach(System.out::println); // печатаем
+        List<Integer> numbers = new ArrayList<>(List.of(1, 9, 2, 8, 3, 7, 4, 6, 5));
+
+        numbers.stream() // открыл стрим
+                .filter(n -> n > 3) // отбираем числа больше 3
+                .peek(n -> System.out.println("peek befor sort: " + n)) // для отлаживания
+                .sorted((n1, n2) -> - Integer.compare(n1, n2) ) // sorting
+                .forEach(System.out::println ); // принт
+
         System.out.println("==============================");
         numbers.forEach(System.out::println); // еще один вариант печати
         System.out.println("==============================");
+
         Integer[] arr = {1, 9, 2, 8, 3, 7, 4, 6, 5};
         Stream<Integer> streamInt = Arrays.stream(arr)
-                .filter(n -> n > 2) // отобрали большие 2
-                .filter(n -> n % 2 == 0) // отобрали только четные
-                .peek(n -> System.out.println("peek: " + n)) // debugger
-                .sorted((n1, n2) -> -Integer.compare(n1, n2)); // отсортировали
-
+                .filter(n -> n>2)
+                .filter(n -> n % 2 == 0) // нужны четные
+                .sorted((n1, n2) -> - Integer.compare(n1, n2));
         streamInt.forEach(System.out::println);
+
         System.out.println("==============================");
-//        streamInt.forEach(System.out::println);
         Integer res = Arrays.stream(arr)
-                .map(n -> n * 10)
-                .reduce(0, (accum, n) ->  accum + n);
+                .map(n -> n * 10) // преобразование
+                .reduce(0, (accum, n) ->  accum + n); // накопление
         System.out.println("sum = " + res);
-        res = Arrays.stream(arr)
-                .reduce(1, (accum, n) ->  accum * n);
-        System.out.println("product = " + res);
+
+        // стринг
         String str = Arrays.stream(arr)
-                .sorted()
-                .map(Object::toString)
+                .sorted((n1,n2) -> - Integer.compare(n1, n2)) // сортировка
+                .map(Object::toString)// преобразование в стринг
                 .reduce("Result = ", (accum, n) ->  accum + n);
         System.out.println(str);
         System.out.println("==============================");
-        Integer[] sortedNum = Arrays.stream(arr)
-                .sorted((n1,n2) -> -Integer.compare(n1,n2))
-//                                    .toArray(size -> new Integer[size]);
-                .toArray(Integer[]::new);
-        Arrays.stream(sortedNum).forEach(System.out::print);
-        System.out.println();
-        System.out.println("==============================");
-        res = Arrays.stream(arr)
-                .peek(n -> System.out.println("n = " + n))
-                .filter(n -> n % 4 == 0)
-                .findFirst()
-                .orElse(Integer.MAX_VALUE);
-        System.out.println(res);
-
     }
+
 }
