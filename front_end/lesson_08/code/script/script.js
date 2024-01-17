ef6fdeb04e743c784d101c66fd266c4e1b3b01a4
HaykInanc <hayk.inanc@gmail.com>
2024-01-17 16:15:08 +0400

    lesson 08 (js)

---
+++ front_end/lesson_08/code/script/script.js
@@ -0,0 +1,111 @@
+// функция для вывода модального окна с подсказкой
+// alert("Моя первая строчка в JS");
+
+// const number_1 = 22;
+// const number_2 = 22;
+
+// if (number_1 > number_2){
+//     console.log(number_1);
+// }else if (number_1 < number_2){
+//     console.log(number_2);
+// }else{
+//     console.log('Они равны');
+// }
+
+// const number = 13;
+
+// if (number % 2 === 0){
+//     console.log("четное");
+// }else{
+//     console.log("не четное");
+// }
+
+
+// const list = [12, 4, 65, 3, 5];
+// list.push(4);
+
+// console.log(list);
+// console.log(list[1]); // получение элемента с индексом 1
+// console.log(list.length);
+
+// let result = 0;
+// for (let i = 0; i < list.length; i++){
+//     result += list[i];
+// }
+
+// console.log(result);
+// доработать цикл таким образом, чтобы он находил 
+// сумму чисел массива и выводил в консоль
+
+// написать цикл, который выводит все значения 
+// массива по одному
+
+// for (let i = 9; i >= 0; i--){
+//     console.log(i);
+// }
+
+// написать цикл, который выводит цифры от 9 до 0
+
+
+
+// написать цикл, который выводит цифры от 0 до 9
+
+// Javascript 
+// DOM (взаимодейтсвие с HTML)
+// NODE.js (терминальный JS)
+
+// ES6 - новая спецификация (2015)
+
+// напишите программу, в которой есть переменная 
+// с числом 
+// используя условный оператор выведите сообщение 
+// "четное" или "не четное" в зависимости от значения
+
+
+// const words = ['ролики', 'самокат', 'велосипед', 'стол'];
+
+// написать цикл, который выводит слова, 
+// которые длиннее 7 символов
+
+// let result = words[0];
+// for (word of words){
+//     if (word.length > result.length){
+//         result = word;
+//     }
+// }
+// console.log(result);
+
+// for (let i = 0; i < words.length; i++){
+//     if (words[i].length > result.length){
+//         result = words[i];
+//     }
+// }
+
+// console.log(result);
+
+
+// написать цикл, который выводит слово с наибольшей 
+// длинной, если таких несколько, нужно вывести первое
+
+
+
+
+
+// let value_1;
+// if (true){
+//     value_1 = 12;
+//     var value_2 = 14;
+// }
+
+// console.log(value_1);
+// console.log(value_2);
+
+for(var i = 0; i < 10; i++){
+    console.log(i);
+}
+
+console.log(i);
+console.log(i);
+console.log(i);
+console.log(i);
+console.log(i);
