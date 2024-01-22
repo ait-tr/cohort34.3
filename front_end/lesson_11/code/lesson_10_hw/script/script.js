b8a54545448b18d2c05e91fd6853994f0e44917d
HaykInanc <hayk.inanc@gmail.com>
2024-01-22 16:38:07 +0400

    lesson_11 (array methods)

---
+++ front_end/lesson_11/code/lesson_10_hw/script/script.js
@@ -0,0 +1,18 @@
+// const array = [1, 4, 2, 6, 4, 7];
+// const result = array.map(elem => elem * 2);
+// console.log(array);
+// console.log(result);
+
+// const array = ["hello", "world"];
+// const result = array.map(elem => elem.toUpperCase());
+// console.log(result);
+
+// let result = 0;
+// const array = [1, 4, 2, 6, 4, 7];
+
+// array.forEach(elem => result += elem);
+// console.log(result);
+
+// const array = [-1, 4, -2, 0, 6, -4, 7];
+// const result = array.filter(elem => elem >= 0);
+// console.log(result);
