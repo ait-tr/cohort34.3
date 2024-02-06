13a72b3082fffd13d528442633acb608276e809a
HaykInanc <hayk.inanc@gmail.com>
2024-02-06 16:00:47 +0400

    lesson_22 (typescript install)

---
+++ front_end/lesson_22/code/lesson_22/public/script.js
@@ -0,0 +1,8 @@
+"use strict";
+const userName = "Гайк";
+const userAge = 31;
+const isTeacher = true;
+function sum(x, y) {
+    return x + y;
+}
+console.log(sum(12, 45));
---
+++ front_end/lesson_22/code/lesson_22/src/script.ts
@@ -0,0 +1,10 @@
+const userName:string = "Гайк";
+const userAge:number = 31;
+const isTeacher:boolean = true;
+
+function sum(x:number, y:number):number{
+    return x + y;
+}
+
+
+console.log(sum(12, 45));
