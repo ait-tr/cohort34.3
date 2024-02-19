41883515295d78bbd07a7ec407e0f2b124cf2aab
haykInant <hayk@berlintech.ai>
2024-02-20 00:33:48 +0400

    lesson_31 (Redux)

---
+++ front_end/lesson_31/code/app/src/store/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./slices/counterSlice";
+
+export const store = configureStore({
+    reducer: {
+        counter: counterReducer
+    }
+});
+
+export type Store = {
+    counter: {
+      value:number
+    }
+}
