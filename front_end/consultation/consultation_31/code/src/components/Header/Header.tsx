7fe2cde9ab6d3935c7345897f662c74430f9c68b
abakharev <abakharev@sharevault.com>
2024-02-19 18:16:44 +0300

    initial

---
+++ front_end/consultation/consultation_31/code/src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      Магазин Классных Вещей{" "}
+      <span style={{ color: "gray" }}>Корзина: 10 товаров</span>
+    </header>
+  );
+}
+
+export default Header;
---
+++ front_end/consultation/consultation_31/code/src/components/Products/Products.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Products() {
+  return (
+    <div className="products">
+      <div className="product">
+        <div>Чайник</div>
+        <div>
+          <button>-</button> 0 <button>+</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+p;
+
+export default Products;
