2accd082d08c23180bb3cbf1041d745ead3f012e
abakharev <abakharev@sharevault.com>
2024-02-21 19:12:19 +0300

    consultation_33

---
+++ front_end/consultation/consultation_33/code/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import Products from "./components/Products/Products";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Products />
+    </div>
+  );
+}
+
+export default App;
